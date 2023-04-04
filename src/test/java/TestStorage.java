4f7d30cf9b13b942d669246331483512d3ce8b58
v sotnikov <sotnikov@rosplatforma.ru>
2023-04-04 12:13:06 +0300

    commit1 04.04.2023

--- src/test/java/TestStorage.java
+++ src/test/java/TestStorage.java
@@ -1,4 +1,6 @@
+import io.qameta.allure.Attachment;
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.json.JSONObject;
 
 public class TestStorage {
@@ -23,5 +25,10 @@ public class TestStorage {
                 .getDetailedCookies().toString();
         cluster_id = 3;
         cluster_name = "newCluster";
+    }
+
+    @Attachment(value = "Response", type = "application/json", fileExtension = ".txt")
+    public static String getBody (Response response) {
+        return response.getBody().prettyPrint();
     }
 }
